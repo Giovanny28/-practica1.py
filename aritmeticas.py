707e541e6657daf9dac6bf16627e04a280760e8f
Giovanny28 <giovannypn28@gmail.com>
2025-11-13 21:37:23 -0600

    Create aritmeticas.py

---
+++ aritmeticas.py
@@ -0,0 +1,22 @@
+# Captura de datos
+num1 = int(input("Ingresa el primer número entero: "))
+num2 = int(input("Ingresa el segundo número entero: "))
+
+# Operaciones aritméticas
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+division_entera = num1 // num2
+modulo = num1 % num2
+potencia = num1 ** num2
+
+# Mostrar resultados
+print("\n--- Resultados ---")
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+print("División entera:", division_entera)
+print("Módulo (residuo):", modulo)
+print("Potencia:", potencia)
