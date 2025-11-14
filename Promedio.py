7c6381836203fe1897ac60ed2c60d4377bc3aeef
Giovanny28 <giovannypn28@gmail.com>
2025-11-13 21:41:05 -0600

    Create Promedio.py

---
+++ Promedio.py
@@ -0,0 +1,5 @@
+1. Captura tres calificaciones decimales ingresadas por el usuario.
+2. Calcula el promedio.
+3. Muestra el promedio en pantalla.
+4. Si el promedio es mayor o igual a 70, muestra el mensaje “¡Felicidades! Aprobaste ”.
+   De lo contrario, muestra “Lo siento, necesitas estudiar más”.
